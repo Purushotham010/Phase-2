0cef5b4e59f95ffc5209bd73183b6a5392d1a5b2
Purushotham010 <purushothamchowdam401@gmail.com>
2026-08-22 14:48:47 +0530

    Added Day_17 files

---
+++ Day_17/five.java
@@ -0,0 +1,39 @@
+package Day_17;
+
+pub//Queue using two stacks
+import java.util.Scanner;
+import java.util.Stack;
+class five {
+    static Stack<Integer> stack1 = new Stack<>();
+    static Stack<Integer> stack2 = new Stack<>();
+
+    static void enqueue(int value) {
+        stack1.push(value);
+    }
+
+    static void dequeue() {
+
+        if (stack2.empty()) {
+
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        if (!stack2.empty()) {
+            System.out.println("Removed: " + stack2.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        enqueue(10);
+        enqueue(20);
+        enqueue(30);
+
+        dequeue();
+        dequeue();
+    }
+}lic class five {
+    
+}
