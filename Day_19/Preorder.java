4da758c46c975fc0d877ba8f5c9a8df3f554cecb
Purushotham <purushothamchowdam401@gmail.com>
2026-08-23 10:31:16 +0530

    Add files via upload

---
+++ Day_19/Preorder.java
@@ -0,0 +1,35 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Preorder {
+    static void preorder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+}class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Preorder {
+    static void preorder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
